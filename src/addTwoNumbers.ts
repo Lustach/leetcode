5c747b66b97f25bc1a74f52ff6fea67aaf080271
Lustach <lusta.vlad2001@gmail.com>
2023-05-06 10:24:13 +0300

    fix

---
+++ src/addTwoNumbers.ts
@@ -0,0 +1,16 @@
+
+// class ListNode {
+//     val: number
+//     next: ListNode | null
+//     constructor(val?: number, next?: ListNode | null) {
+//       this.val = (val === undefined ? 0 : val)
+//       this.next = (next === undefined ? null : next)
+//     }
+//   }
+
+
+// function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | null {
+//     let prepL1 = +(l1.reverse().join(''));
+//     let prepL2 = +(l2.reverse().join(''));
+//     return String(prepL1 + prepL2).split('').reverse()
+//   };
